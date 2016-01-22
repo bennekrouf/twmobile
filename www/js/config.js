034277223b026d191e18b112425b613040bcaf45
bennekrouf <mohamed.bennekrouf@b-i.com>
2016-01-22 11:58:56 +0100

    changes stles

--- www/js/config.js
+++ www/js/config.js
@@ -11,7 +11,7 @@ angular.module('nibs.config', [])
     ])
 
     .constant('STATUS_DESCRIPTIONS', [
-        'Member - Access to members only store and exclusive sales. All members get 5% off!',
-        'Premium Member - Access to premium members only store and exclusive sales. All premium members get 15% off!',
+        'Silver Member- Access to silver members only store and exclusive sales. All members get 5% off!',
+        'Gold Member - Access to premium members only store and exclusive sales. All premium members get 15% off!',
         'Platinum Member - Access to platinum members only store and exclusive sales. All platinum members get 25% off!'
     ]);
