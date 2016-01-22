04e700d3fc34d2f4ebb1523eb08a4b4ce497ba14
bennekrouf <mohamed.bennekrouf@b-i.com>
2016-01-22 12:10:57 +0100

    add camera plugin

--- www/js/config.js
+++ www/js/config.js
@@ -11,7 +11,7 @@ angular.module('nibs.config', [])
     ])
 
     .constant('STATUS_DESCRIPTIONS', [
-        'Silver Member- Access to silver members only store and exclusive sales. All members get 5% off!',
-        'Gold Member - Access to premium members only store and exclusive sales. All premium members get 15% off!',
+        'Silver Member- Access to silver members only store and exclusive sales. All silver members get 5% off!',
+        'Gold Member - Access to premium members only store and exclusive sales. All gold members get 15% off!',
         'Platinum Member - Access to platinum members only store and exclusive sales. All platinum members get 25% off!'
     ]);
